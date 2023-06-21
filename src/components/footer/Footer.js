15d5426879ecaad4377fda29fb6bfee5c021f226
Fikri Ramadianto <ramadiantofikri@gmail.com>
2023-06-21 21:30:56 +0700

    laayout-navbar-footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+"use client"
+
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+        <div>©2023 insightBlog. All rights reserved.</div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
